102d38bf4ff172bb673e3fe8660984c1574ea037
jadg13 <jadg13@hotmail.com>
2025-03-31 11:59:07 -0600

    Agregue el ejericicio de calculo de compra

---
+++ example2.py
@@ -0,0 +1,9 @@
+""" 
+Calcular el monto a pagar por x cantidad de producto 
+a x precio 
+"""
+
+cant_prod = int(input("Dime la cantidad de producto: "))
+precio_prod = float(input("Dime el precio del producto: "))
+monto = cant_prod * precio_prod
+print(f"El monto a pagar es: {monto}")
